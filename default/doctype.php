c34f1a465bacd5f6514646b2c9b7f5778ce828da
David Tucker <david.michael.tucker@gmail.com>
2014-07-07 08:42:03 -0700

    Organize archive.

--- default/doctype.php
+++ default/doctype.php
@@ -6,7 +6,7 @@
 	<!--meta name="revised" content="<?php echo date("r",filemtime(__FILE__)); ?>" /-->
 	<meta name="google-site-verification" content="1gwimH0cYGn-6E0ByBejO8Q7gzvwzL_TMxgjFlm8P6Y" />
 	
-	<title>Joyce Freeman, B.S., CCHT</title>
+	<title>Joyce Freeman BS, CCHT</title>
 	
 	<link rel="shortcut icon" href="/resources/images/title.ico" />
 	<link rel="stylesheet" type="text/css" href="/resources/style.css" />
